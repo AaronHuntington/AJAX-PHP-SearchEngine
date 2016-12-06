5222e7d01be921c2897029403a90e680e4f07723
AaronHuntington <8ahuntington@gmail.com>
2016-12-05 18:30:16 -0600

    Added database.php, bootstrap, and more

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>AJAX - PHP - Search Engine Template</title>
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    </head>
+    <body>
+    	<section class="row">
+    		<div class="col-md-8 col-md-offset-2">
+    			<h1>Search the Database</h1>
+    		</div>
+    	</section>
+    </body>
+</html>
